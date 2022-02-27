d61e902478c26e223700878492d9dd4f589ec7ac
vijay.k.arora <vijay.k.arora@ril.com>
2022-02-27 12:46:24 +0530

    Problem solved

--- LongestSubstring/main.go
+++ LongestSubstring/main.go
@@ -11,43 +11,22 @@ func main() {
 }
 
 func lengthOfLongestSubstring(s string) int {
-	// Length of the given input string
 	n := len(s)
-
-	// Length of longest substring
 	var result int
-
-	// Map to store visited characters along with their index
 	charIndexMap := make(map[uint8]int)
-
-	// start indicates the start of current substring
 	var start int
-
-	// Iterate through the string and slide the window each time you find a duplicate
-	// end indicates the end of current substring
 	for end := 0; end < n; end++ {
-
-		// Check if the current character is a duplicate
 		duplicateIndex, isDuplicate := charIndexMap[s[end]]
 		if isDuplicate {
-			// Update the result for the substring in the current window before we found duplicate character
 			result = max(result, end-start)
-
-			// Remove all characters before the new
 			for i := start; i <= duplicateIndex; i++ {
 				delete(charIndexMap, s[i])
 			}
-
-			// Slide the window since we have found a duplicate character
 			start = duplicateIndex + 1
 		}
-		// Add the current character to hashmap
 		charIndexMap[s[end]] = end
 	}
-	// Update the result for last window
-	// For a input like "abc" the execution will not enter the above if statement for checking duplicates
 	result = max(result, n-start)
-
 	return result
 }
 
